e69fe1fa2f9fb4509bf21605320a1565252ad2a0
Nathan Byrne <nathan_byrne96@hotmail.co.uk>
2019-02-07 22:29:38 +0000

    added conditionals

--- TPT/conditionals.php
+++ TPT/conditionals.php
@@ -1,5 +1,5 @@
 <?php
-    #CONDITIONALS
+    #CONDITIONALS ---------------------
 
     /*
         ==      equals
@@ -22,6 +22,50 @@
     }
     else{
         echo "the number is: $num";
+    }
+
+
+    #LOGICAL OPERATORS --------------------
+
+    /*
+        and     &&      all conditions must be met
+        or      ||      any condition can be met
+        xor             only one condition can be met
+    */
+
+    $num = 11;
+
+    if ($num > 4 && $num < 10){
+        echo "$num passed: and";
+    }
+
+    if ($num > 4 || $num < 10){
+        echo "$num passed: or";
+    }
+
+    if ($num > 4 xor $num < 10){
+        echo "$num passed: xor";
+    }
+
+    echo "<br>";
+
+
+    #SWITCH CASE ----------------------------
+
+    $faveGame = "cod bo4";
+
+    switch($faveGame){
+        case "apex legends":
+            echo "$faveGame is the best BR game!!";
+            break;
+        case "fortnite":
+            echo "$faveGame is overated, nice map tho";
+            break;
+        case "PUBG":
+            echo "$faveGame isn't a bad game, runs like shit tho";
+            break;
+        default:
+            echo "$faveGame is shit lol";
     }
 
 ?>
